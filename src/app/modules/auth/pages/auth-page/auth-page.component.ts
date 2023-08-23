0c0f97726a0f6ca2d4d02b3ce5ee014c535905d9
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2023-08-22 19:43:36 -0500

    spotify

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -14,7 +14,7 @@ export class AuthPageComponent implements OnInit{
   formLogin:FormGroup = new FormGroup({});
   viewMessageError:boolean=false;
 
-  constructor(private _authService:AuthService, private  router:Router){
+  constructor(private _authService:AuthService, private  router:Router,private cookie: CookieService){
 
   }
 
@@ -37,16 +37,22 @@ export class AuthPageComponent implements OnInit{
     // console.log(this.formLogin)
   }
 
-  send():void{
-    const {email,password} = this.formLogin.value;
-    this.viewMessageError = this.formLogin.invalid;
-    this._authService.sendCredentials(email,password).subscribe((resOk)=>{
-      // console.log(resOk)
-      this.router.navigate(['/','tracks'])
-    }, err =>{
-      this.errorInitSession = true;
-      console.log(err)
-    });
+   send(): void {
+    const { email, password } = this.formLogin.value
+    this._authService.sendCredentials(email, password)
+      //TODO: 200 <400
+      .subscribe(responseOk => { //TODO: Cuando el usuario credenciales Correctas ✔✔
+        console.log('Session iniciada correcta', responseOk);
+        const { tokenSession, data } = responseOk
+        this.cookie.set('token', tokenSession, 4, '/') //TODO:📌📌📌📌
+        this.router.navigate(['/', 'tracks'])
+      },
+        err => {//TODO error 400>=
+          this.errorInitSession = true
+          console.log('⚠⚠⚠⚠Ocurrio error con tu email o password');
+        })
+
   }
+
 
 }
--- src/app/modules/auth/service/auth.service.ts
+++ src/app/modules/auth/service/auth.service.ts
@@ -13,16 +13,13 @@ export class AuthService {
 
   constructor(private http:HttpClient,private cookie:CookieService) { }
 
-  sendCredentials(email:string,password:string):Observable<any>{
+ sendCredentials(email: string, password: string): Observable<any> {
     const body = {
       email,
       password
     }
-    return this.http.post(`${this.URL}/auth/login`,body).pipe(
-      tap((responseOk:any)=>{
-        const {tokenSession,data}= responseOk;
-        this.cookie.set('token',tokenSession,4,'/')
-      })
-    )
+    console.log(this.URL)
+    return this.http.post(`${this.URL}/auth/login`, body)
   }
+
 }
